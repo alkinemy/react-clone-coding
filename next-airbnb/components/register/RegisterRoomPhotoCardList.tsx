b91532a7e91657bb36402fe42867888b4dedb46a
alkinemy <alta.second@gmail.com>
2021-11-26 23:08:31 -0500

    chapter17: upload multiple photos

--- next-airbnb/components/register/RegisterRoomPhotoCardList.tsx
+++ next-airbnb/components/register/RegisterRoomPhotoCardList.tsx
@@ -3,6 +3,11 @@ import palette from "../../styles/palette";
 import React from "react";
 import PencilIcon from "../../public/static/svg/register/pencel.svg";
 import TrashCanIcon from "../../public/static/svg/register/trash_can.svg";
+import GrayPlusIcon from "../../public/static/svg/register/gray_plus.svg";
+import {uploadFileAPI} from "../../lib/api/file";
+import {useDispatch} from "react-redux";
+import {registerRoomActions} from "../../store/registerRoom";
+import RegisterRoomFooter from "./RegisterRoomFooter";
 
 
 const Container = styled.ul`
@@ -107,7 +112,48 @@ interface IProps {
 }
 
 const RegisterRoomPhotoCardList: React.FC<IProps> = ({photos}) => {
-
+    const dispatch = useDispatch();
+    const addPhoto = () => {
+        const el = document.createElement("input");
+        el.type = "file";
+        el.accept = "image/*";
+        el.onchange = (event) => {
+            const {files} = event.target as HTMLInputElement;
+            if (files && files.length > 0) {
+                const file = files[0];
+                const formData = new FormData();
+                formData.append("file", file);
+                uploadFileAPI(formData)
+                .then(({data}) => {
+                    dispatch(registerRoomActions.setPhotos([...photos, data]));
+                }).catch((e) => console.log(e));
+            }
+        };
+        el.click();
+    };
+    const deletePhoto = (index: number) => {
+        const newPhotos = [...photos];
+        newPhotos.splice(index, 1);
+        dispatch(registerRoomActions.setPhotos(newPhotos));
+    };
+    const editPhoto = (index: number) => {
+        const el = document.createElement("input");
+        el.type = "file";
+        el.onchange = (event) => {
+            const file = (event.target as HTMLInputElement)?.files?.[0];
+            if (file) {
+                const formData = new FormData();
+                formData.append("file", file);
+                uploadFileAPI(formData)
+                    .then(({data}) => {
+                        const newPhotos = [...photos];
+                        newPhotos[index] = data;
+                        dispatch(registerRoomActions.setPhotos(newPhotos));
+                    }).catch((e) => console.log(e.message));
+            }
+        }
+        el.click();
+    };
     return (
         <Container>
             {photos.map((photo, index) => (
@@ -116,10 +162,23 @@ const RegisterRoomPhotoCardList: React.FC<IProps> = ({photos}) => {
                         <li className="register-room-first-photo-wrapper">
                             <img src={photo} alt=""/>
                             <div className="register-room-photo-interaction-buttons">
-                                <button type="button" onClick={() => {}}>
+                                <button type="button" onClick={() => deletePhoto(index)}>
                                     <TrashCanIcon/>
                                 </button>
-                                <button type="button" onClick={() => {}}>
+                                <button type="button" onClick={() => editPhoto(index)}>
+                                    <PencilIcon/>
+                                </button>
+                            </div>
+                        </li>
+                    )}
+                    {index !== 0 && (
+                        <li className="register-room-photo-card">
+                            <img src={photo} alt=""/>
+                            <div className="register-room-photo-interaction-buttons">
+                                <button type="button" onClick={() => deletePhoto(index)}>
+                                    <TrashCanIcon/>
+                                </button>
+                                <button type="button" onClick={() => editPhoto(index)}>
                                     <PencilIcon/>
                                 </button>
                             </div>
@@ -127,6 +186,20 @@ const RegisterRoomPhotoCardList: React.FC<IProps> = ({photos}) => {
                     )}
                 </React.Fragment>
             ))}
+            <li
+                className="register-room-photo-card"
+                role="presentation"
+                onClick={addPhoto}
+            >
+                <div className="register-room-add-more-photo-card">
+                    <GrayPlusIcon/>
+                    추가하기
+                </div>
+            </li>
+            <RegisterRoomFooter
+                prevHref="/room/register/conveniences"
+                nextHref="/room/register/description"
+            />
         </Container>
     );
 }
